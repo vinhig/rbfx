301c34bc08b36e5667c65f798ecb6a878b8b5f9e
Eugene Kozlov <nekoeuge@gmail.com>
2020-06-26 23:43:31 +0300

    Fix build.

--- Source/Urho3D/Graphics/OpenGL/OGLGraphics.cpp
+++ Source/Urho3D/Graphics/OpenGL/OGLGraphics.cpp
@@ -1179,6 +1179,7 @@ void Graphics::SetShaderConstantBuffers(ea::span<const ConstantBufferRange, MAX_
         return;
     }
 
+#ifndef GL_ES_VERSION_2_0
     for (unsigned i = 0; i < MAX_SHADER_PARAMETER_GROUPS; ++i)
     {
         const ConstantBufferRange& range = constantBuffers[i];
@@ -1190,6 +1191,7 @@ void Graphics::SetShaderConstantBuffers(ea::span<const ConstantBufferRange, MAX_
             constantBuffers_[i] = range;
         }
     }
+#endif
 }
 
 void Graphics::SetShaderParameter(StringHash param, const float data[], unsigned count)
